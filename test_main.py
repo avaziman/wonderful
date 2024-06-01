61cfd3f95ce000b3772d35fc51863e417d69ed23
ISickMan <adam.vaizman@gmail.com>
2024-06-01 21:25:39 +0300

    done

--- test_main.py
+++ test_main.py
@@ -7,15 +7,14 @@ client = TestClient(app)
 
 
 def test_transcribe():
-    # file = open('./example.flac', 'rb')
-    files = {'file': ("example.flac", open('example.flac', 'rb'), "audio/flac")}
+    files = {'file': ("example.flac", open('./example.flac', 'rb').read(), "audio/flac")}
     response = client.post("/transcribe", files=files)
 
     checksum = "424522392f1d790c335720b2ae6c92ba"
     assert response.status_code == 200
     assert response.json() == {"checksum": checksum}
     
-    sleep(200)
-    response = client.get('/result/{}'.format(checksum))
+    # sleep(1)
+    response = client.get('/result/{}.srt'.format(checksum))
 
     assert response.status_code == 200
--- transcriber.py
+++ transcriber.py
@@ -2,34 +2,50 @@ import hashlib
 from queue import Queue
 import threading
 from fastapi import UploadFile
+import torch
 import whisper
 from whisper.utils import get_writer
 
 transcribe_queue = Queue()
 
 class TranscribeJob:
-    def __init__(self, file: UploadFile):
-        self.checksum = get_checksum(file)
-        self.file = file
+    def __init__(self, src: str, checksum: str):
+        self.checksum = checksum 
+        self.src = src
 
-# blocking  (bad)
-def get_checksum(file: UploadFile) -> str:
-    return hashlib.md5(file.file.read()).hexdigest()
+# can be split
+async def get_checksum(file: UploadFile) -> str:
+    abytes = await file.read()
+    await file.seek(0)
+    return hashlib.md5(abytes).hexdigest()
 
 async def add_job(file: UploadFile) -> str:
-    job = TranscribeJob(file)
+    checksum = await get_checksum(file); 
+    # save in fs
+    
+    src = './results/src/{}'.format(file.filename)
+    with open(src, 'wb') as f:
+        src_bytes = await file.read()
+        f.write(src_bytes)
+
+    job = TranscribeJob(src, checksum)
     transcribe_queue.put(job)
+
+
     return job.checksum
 
 def work_loop():
-    # block until theres job
     model = whisper.load_model("tiny")
+    print("CUDA IS AVAIL", torch.cuda.is_available())
+    print(torch.cuda.get_device_name(0))
     print("Worker running")
-    while True:
-        job = transcribe_queue.get(True, None)
-        print("Working on job", job)
 
-        result = model.transcribe(job.file.file)
+    while True:
+        # block until theres job
+        job = transcribe_queue.get(True, None)
+        print("Working on job", job.src)
+
+        result = model.transcribe(job.src)
 
         print("Finished working on job")
         writer = get_writer("srt", output_dir="./results")
